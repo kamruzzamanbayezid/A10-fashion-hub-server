5d74b2f23a32e5232a8a9a11464c0ff4491121c8
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-10-19 00:03:03 +0600

    get all products using find method

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ async function run() {
 
             const productsCollection = client.db("ProductsDB").collection("products");
 
+            app.get('/products', async (req, res) => {
+                  const cursor = productsCollection.find()
+                  const result = await cursor.toArray()
+                  res.send(result);
+            })
+
             app.post('/products', async (req, res) => {
                   const products = req.body;
                   const result = await productsCollection.insertOne(products);
