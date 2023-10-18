1264c18e314479d9ea6ea67e5d27a7ecb5c04041
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-10-18 23:40:34 +0600

    cached .env

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d8abmis.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
       serverApi: {
             version: ServerApiVersion.v1,
@@ -26,12 +24,21 @@ async function run() {
       try {
             // Connect the client to the server	(optional starting in v4.7)
             await client.connect();
+
+            const productsCollection = client.db("ProductsDB").collection("products");
+
+            app.post('/products', async (req, res) => {
+                  const products = req.body;
+                  const result = await productsCollection.insertOne(products);
+                  res.send(result)
+            })
+
             // Send a ping to confirm a successful connection
             await client.db("admin").command({ ping: 1 });
             console.log("Pinged your deployment. You successfully connected to MongoDB!");
       } finally {
             // Ensures that the client will close when you finish/error
-            await client.close();
+            // await client.close();
       }
 }
 run().catch(console.dir);
